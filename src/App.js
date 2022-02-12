cf6977443ac1e43a6c0876cc6f28f234886a4487
smitalpatil17 <smitalpatil17@gmail.com>
2022-02-12 20:07:30 +0530

    Created Sign In Page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
-
+import SignInSignOut from './pages/sign-in-sign-out/sign-in-sign-out.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Routes>
         <Route exact path='/' element={<HomePage/>} />
         <Route path='/shop' element={<ShopPage/>} />
+        <Route path='/signin' element={<SignInSignOut/>} />
       </Routes>
     </div>
   );
---
+++ src/pages/sign-in-sign-out/sign-in-sign-out.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './sign-in-sign-out.styles.scss'
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInSignOut =()=>(
+    <div className="sign-in-sign-out"> <SignIn></SignIn> </div>
+)
+
+export default SignInSignOut;
